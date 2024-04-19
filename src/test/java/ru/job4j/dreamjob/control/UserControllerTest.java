e3ac6358cb3ba3e1ee0eb492b6573f14b7e0103b
Lyubov <n.lubovj@gmail.com>
2024-04-19 08:19:38 +0300

    Add new tests to UserControllerTest class

--- src/test/java/ru/job4j/dreamjob/control/UserControllerTest.java
+++ src/test/java/ru/job4j/dreamjob/control/UserControllerTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.dreamjob.control;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.ui.ConcurrentModel;
 import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.service.UserService;
@@ -45,11 +47,53 @@ class UserControllerTest {
     }
 
     @Test
-    void getLoginPage() {
+    void registerWhenEmailAlreadyExistsThenGetErrorPageWithMessage() {
+        var expectedMessage = "Пользователь с таким email уже существует";
+        when(userService.save(any())).thenReturn(Optional.empty());
+
+        var model = new ConcurrentModel();
+        var view = userController.register(model, new User());
+        var actualMessage = model.getAttribute("message");
+
+        assertThat(view).isEqualTo("errors/404");
+        assertThat(actualMessage).isEqualTo(expectedMessage);
     }
 
     @Test
-    void loginUser() {
+    void getLoginPage() {
+        String expectedView = "users/login";
+
+        String view = userController.getLoginPage();
+
+        assertThat(view).isEqualTo(expectedView);
+    }
+
+    @Test
+    void whenLoginUserSuccessfullyThenRedirectVacanciesPage() {
+        User user = new User(1, "email", "name", "password");
+        when(userService.findByEmailAndPassword(any(), any())).thenReturn(Optional.of(user));
+        HttpServletRequest request = new MockHttpServletRequest();
+
+        var model = new ConcurrentModel();
+        var view = userController.loginUser(user, model, request);
+        var actualUser = request.getSession().getAttribute("user");
+
+        assertThat(view).isEqualTo("redirect:/vacancies");
+        assertThat(actualUser).usingRecursiveComparison().isEqualTo(user);
+    }
+
+    @Test
+    void loginUserWithIncorrectDataThenGeLoginPageWithErrorMessage() {
+        when(userService.save(any())).thenReturn(Optional.empty());
+        HttpServletRequest request = new MockHttpServletRequest();
+        String expectedMessage = "Почта или пароль введены неверно";
+
+        var model = new ConcurrentModel();
+        var view = userController.loginUser(new User(), model, request);
+        var actualMessage = model.getAttribute("error");
+
+        assertThat(view).isEqualTo("users/login");
+        assertThat(actualMessage).isEqualTo(expectedMessage);
     }
 
     @Test
